d4120f1f707d9ba2dc67b4d1509b2b6406c3b26c
mohamed hossam <101831478+mohamed1092001@users.noreply.github.com>
2024-09-24 14:28:19 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-$(".oppening .box .logo-text img").css("width","220px")
+$(".oppening .box .logo-text img").css("width", "220px")
 $(".oppening .box .logo-text p").css({ "width": "230px", "transition": "1.2s" })
 
-setTimeout(function(){
+setTimeout(function () {
     $(".oppening .box .logo-text p").css({ "height": "22px", "transition": ".8s" })
-},1500)
+}, 1500)
+
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.overflow = "hidden";
+        document.querySelector(".oppening").style.visibility = "visible";
+    } else {
+        setTimeout(function () {
+            $(".oppening").fadeOut();
+            document.querySelector("body").style.overflow = "visible";
+        }, 2500)
+    }
+};
