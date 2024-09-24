02b2b8f6d07689e0f498d143f7f839128744dd40
mohamed hossam <101831478+mohamed1092001@users.noreply.github.com>
2024-09-24 14:17:18 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(".oppening .box .logo-text img").css("width","220px")
+$(".oppening .box .logo-text p").css({ "width": "270px", "transition": "1.2s" })
+
+setTimeout(function(){
+    $(".oppening .box .logo-text p").css({ "height": "22px", "transition": ".8s" })
+},1500)
